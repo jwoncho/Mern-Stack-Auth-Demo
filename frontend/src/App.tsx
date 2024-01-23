30958a1f3ebe17491f561f00a423075bb603e10b
jwoncho <jaewon0907@gmail.com>
2024-01-23 06:17:27 -0500

    Adding note functionality added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Note as NoteModel } from './models/note';
+import Note from './components/Notes';
+import styles from "./styles/NotesPage.module.css"
+import stylesUtils from "./styles/utils.module.css"
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from './components/AddNoteDialog';
+
+function App() {
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const notes = await NotesApi.fetchNotes();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return (
+    <Container>
+      <Button
+        className={`mb-4 ${stylesUtils.blockCenter}`}
+        onClick={() => setShowAddNoteDialog(true)}>
+          Add New Note
+      </Button>
+      <Row xs={1} md={2} xl={3} className='g-4'>
+        {notes.map(note => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+      { showAddNoteDialog &&
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote])
+            setShowAddNoteDialog(false);
+          }}
+        />
+      }
+    </Container>
+  );
+}
+
+export default App;
